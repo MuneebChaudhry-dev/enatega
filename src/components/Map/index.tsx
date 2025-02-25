f8c68c84ca36936ed57ff09f18b26e2c0445ea22
Muneeb Ahmad <muneebchaudhry.dev@gmail.com>
2025-02-25 17:44:36 +0500

    Add the marker

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,26 +1,31 @@
-'use client';
 import { RootState } from '@/store/mapStore';
-import { GoogleMap } from '@react-google-maps/api';
+import { GoogleMap, Marker } from '@react-google-maps/api'; // Import Marker
 import React, { ReactNode } from 'react';
 import { useSelector } from 'react-redux';
+
 export const defaultMapContainerStyle = {
   width: '100%',
   height: '80vh',
 };
+
 const defaultMapCenter = {
   lat: 32.73031773012761,
   lng: 37.838860969312684,
 };
+
 const defaultMapZoom = 10;
+
 const defaultMapOptions = {
   zoomControl: true,
   tilt: 0,
   gestureHandling: 'auto',
   mapTypeId: 'terrain',
 };
+
 interface MapProps {
   children?: ReactNode;
 }
+
 export const Map = ({ children }: MapProps) => {
   const { selectedCity } = useSelector((state: RootState) => state.location);
 
@@ -36,6 +41,10 @@ export const Map = ({ children }: MapProps) => {
         zoom={defaultMapZoom}
         options={defaultMapOptions}
       >
+        {selectedCity && (
+          <Marker position={{ lat: selectedCity.lat, lng: selectedCity.lng }} />
+        )}
+
         {children}
       </GoogleMap>
     </div>
